cbd157e64456afe6ea1e09030a1ac3b96aed145f
Edanur Özdemir <edanurozdemir23@gmail.com>
2022-08-22 17:53:57 +0300

    Update db_connection.php

--- tremglobalphp/db_connection.php
+++ tremglobalphp/db_connection.php
@@ -6,10 +6,3 @@ $database= "tremglobal-task";
 
 // Create connection
 $db_conn = mysqli_connect($servername, $username, $password, $database);
-
-// Check connection
-if ($db_conn->connect_error) {
-    die("Connection failed: " . $db_conn->connect_error);
-}
-echo "Connected successfully";
-?>
